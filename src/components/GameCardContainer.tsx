62d5c4b5c28dd2ccc61da652195a0bc560c4be67
Pouya <p.keubex.83@gmail.com>
2024-05-16 10:15:53 +0330

    fetch Genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -8,7 +8,7 @@ interface Props{
 
 const GameCardContainer = ({children} : Props) => {
   return (
-    <Box width={"300px"} borderRadius={15} overflow={'hidden'}>
+    <Box width="260px" borderRadius={15} overflow={'hidden'}>
         {children}
     </Box>
   )
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import { CardBody,Card,Skeleton, SkeletonText } from '@chakra-ui/react'
 const GameCardSkeleton = () => {
   return (
     <Card >
-        <Skeleton height={"200px"}/>
+        <Skeleton height="200px"/>
         <CardBody>
             <SkeletonText/>
         </CardBody>
