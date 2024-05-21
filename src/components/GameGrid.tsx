ed946d1f99ee538be4e1ad30ab50525d18f9a456
Pouya <p.keubex.83@gmail.com>
2024-05-21 12:10:22 +0330

    Refactor : Game Grid error

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,10 +13,10 @@ interface Props{
 const GameGrid = ({gameQuery }:Props ) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const Skeletons = [1, 2, 3, 4, 5, 6];
-  return (
-    <>
-      {error && <Text>{error}</Text>}
 
+    if(error) return <Text>{error}</Text>
+
+  return (
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={"10px"}
@@ -33,7 +33,7 @@ const GameGrid = ({gameQuery }:Props ) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
+    
   );
 };
 
